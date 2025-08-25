d67b32e8384c2b655dc5b42386bc812ca8d24be0
ShoveX <eduardo.cub.cast@gmail.com>
2025-08-25 16:57:04 -0500

    Base de datos conectada y funcional(django)

--- backend/innova_inversiones/settings.py
+++ backend/innova_inversiones/settings.py
@@ -12,19 +12,31 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+# --- Configurar django-environ ---
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+# Leer archivo .env que está en backend/.env
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-knw=rb##&a&d5ai4+_!)zvjrmnps_e&&a$(_v2y-#qje_o+oz*'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -47,7 +59,8 @@ INSTALLED_APPS = [
     # Local apps
     'properties',
     'users',
-    'maps',
+    'apps.maps',
+    'database',
 ]
 
 MIDDLEWARE = [
@@ -85,10 +98,7 @@ WSGI_APPLICATION = 'innova_inversiones.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),  # lee DATABASE_URL del .env
 }
 
 
