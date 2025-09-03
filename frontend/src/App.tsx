93deb38691162d295fd536b70e5196d45b55643b
Eduardo <eduardo.cub.cast@gmail.com>
2025-09-03 16:45:31 -0500

    Header añadido(aun falta mejorar los estilos)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
 
 import './App.css'
 import MapaLotesPanel from './components/MapaLotesPanel.tsx'
-<MapaLotesPanel />
+import Header from './components/Header.tsx'
 
 function App() {
 
   return (
     <>
       <div>
-        <h1 className="text-white text-4xl font-bold">Mapa de Lotes</h1>
+        <Header />
+      </div>
+      <div>
+        <h1 className="text-Black text-4xl font-bold">Mapa de Lotes</h1>
         <MapaLotesPanel />
       </div>
       <div>
-        <h2 className="text-white text-4x1 font-bold">Panel de informacion</h2>
+        <h2 className="text-white text-4xl font-bold">Panel de informacion</h2>
         
       </div>
     </>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,8 @@ export default {
   content: [
     "index.html",
     "./src/components/mapa_lotes.{js,ts,jsx,tsx}",
+    "./src/components/Header.{js,ts,jsx,tsx}",
+    "./src/components/info_panel.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
