d0c6a6ed3485a1cc9411442284b0fb7da5b9f093
Eduardo <eduardo.cub.cast@gmail.com>
2025-09-25 15:39:58 -0500

    configuracion de backend para admin terminada 1.0

--- backend/apps/administrator/views.py
+++ backend/apps/administrator/views.py
@@ -38,28 +38,52 @@ def Admin_view_lote_codigo(request):
 @permission_classes([AllowAny])
 def AdminUpdateLote(request):
     codigo = request.query_params.get("codigo") or request.data.get("codigo")
-    if not codigo:
-        return Response({"error": "Debe enviar un código"}, status=status.HTTP_400_BAD_REQUEST)
 
-    try:
-        lote = Lote.objects.get(codigo__iexact=codigo)
-    except Lote.DoesNotExist:
-        return Response({"error": "Lote no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+    lote = Lote.objects.get(codigo__iexact=codigo)
 
-    raw = request.data.get("input_estado")
-    if raw is None:
-        return Response({"error": "input_estado requerido"}, status=status.HTTP_400_BAD_REQUEST)
+    raw_estado = request.data.get("input_estado")
+    raw_area_lote = request.data.get("input_area_lote")
+    raw_perimetro = request.data.get("input_perimetro")
+    raw_precio = request.data.get("input_precio")
+    raw_precio_metro_cuadrado = request.data.get("input_precio_metro_cuadrado")
+    raw_descripcion = request.data.get("input_descripcion")
 
-    try:
-        nuevo_estado_id = int(raw)
-    except (TypeError, ValueError):
-        return Response({"error": "input_estado inválido"}, status=status.HTTP_400_BAD_REQUEST)
+    if raw_estado is None:
+        pass
+    else:
+        nuevo_estado_id = int(raw_estado)
+        lote.estado_id = nuevo_estado_id
 
-    if lote.estado_id == nuevo_estado_id:
-        return Response({"message": "Sin cambios (mismo estado)"}, status=status.HTTP_200_OK)
+    if raw_area_lote is None:
+        pass
+    else:
+        nuevo_area_lote = float(raw_area_lote)
+        lote.area_lote = nuevo_area_lote
+
+    if raw_perimetro is None:
+        pass
+    else:
+        nuevo_perimetro = float(raw_perimetro)
+        lote.perimetro = nuevo_perimetro
+
+    if raw_precio is None:
+        lote.precio = float(0)
+    else:
+        nuevo_precio = float(raw_precio)
+        lote.precio = nuevo_precio  
+
+    if raw_precio_metro_cuadrado is None:
+        lote.precio_metro_cuadrado = float(0)
+    else:
+        nuevo_precio_metro_cuadrado = float(raw_precio_metro_cuadrado)
+        lote.precio_metro_cuadrado = nuevo_precio_metro_cuadrado
+
+    if raw_descripcion is None:
+        lote.descripcion = None
+    else:
+        lote.descripcion = raw_descripcion
 
     # asignación correcta del FK
-    lote.estado_id = nuevo_estado_id
-    lote.save(update_fields=["estado", "actualizado_en"])
+    lote.save(update_fields=["estado", "area_lote", "perimetro", "precio", "precio_metro_cuadrado", "descripcion", "actualizado_en"])
 
     return Response({"message": "Lote actualizado correctamente"}, status=status.HTTP_200_OK)
