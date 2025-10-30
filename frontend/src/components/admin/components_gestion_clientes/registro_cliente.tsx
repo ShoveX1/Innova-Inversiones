09839762b644139a338fadda1947edb50038fa6b
Eduardo <eduardo.cub.cast@gmail.com>
2025-10-30 17:31:14 -0500

    Creando formulario para el registro de clientes

--- frontend/src/components/admin/components_gestion_clientes/registro_cliente.tsx
+++ frontend/src/components/admin/components_gestion_clientes/registro_cliente.tsx
@@ -1,4 +1,17 @@
+import { clientesApi } from '@/services';
 
+
+interface Cliente {
+    id: string;
+    nombre: string;
+    apellidos: string;
+    dni: string;
+    direccion: string;
+    telefono: string;
+    email: string;
+    fecha_nacimiento: string;
+    estado: boolean;
+}
 
 export default function RegistroCliente(){
     return (
